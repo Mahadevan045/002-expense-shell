ded490089639bc8814401777e050a1407f733f5b
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-28 20:41:00 +0530

    expense-shell

--- frontend.sh
+++ frontend.sh
@@ -7,15 +7,17 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
+#echo "User ID is : $USERID"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
+
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        echo -e "$R Please run this script with root previliges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -23,40 +25,36 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        echo -e "$2 is ...$R FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is ...$G Success $N" | tee -a $LOG_FILE
     fi
 }
 
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+echo "script started executing at : $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "Installing Nginx"
+VALIDATE $? "install nginx"
 
 systemctl enable nginx &>>$LOG_FILE
-VALIDATE $? "Enable Nginx"
+VALIDATE $? "enable nginx"
 
 systemctl start nginx &>>$LOG_FILE
-VALIDATE $? "Start Nginx"
+VALIDATE $? "start nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-VALIDATE $? "Removing default website"
+VALIDATE $? "removing default website"
+
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
-VALIDATE $? "Downloding frontend code"
+VALIDATE $? "downloading frontend code"
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "Extract frontend code"
+VALIDATE $? "Extracting frontend code"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
-VALIDATE $? "Copied expense conf"
-
-systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "Restarted Nginx"
-
-
+systemctl restart nginx
