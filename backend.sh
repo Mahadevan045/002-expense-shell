9506b6091437039f6c762dfd5e34bf9be63568f1
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-28 20:05:19 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -69,5 +69,21 @@ VALIDATE $? "Extracting backend application code"
 
 
 npm install  &>>$LOG_FILE
-pwd
-#cp /home/ec2-user/002-expense-shell/backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/002-expense-shell/backend.service /etc/systemd/system/backend.service
+
+#load the data before running backend
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MYSQL Client"
+
+
+mysql -h mysql.daws90s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema Loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "enable backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "restart backend"
